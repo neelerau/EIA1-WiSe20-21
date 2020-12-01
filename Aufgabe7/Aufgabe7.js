050cc62774024efa284a2ed050af1036417c6199
neelerau <72865700+neelerau@users.noreply.github.com>
2020-12-01 22:15:05 +0100

    CSS und TS erstellt

--- Aufgabe7/Aufgabe7.js
+++ Aufgabe7/Aufgabe7.js
@@ -1,1 +1,21 @@
+// Soundelemente
+var sound = new Audio('assets/A.mp3');
+sound.play();
+var sound = new Audio('assets/C.mp3');
+sound.play();
+var sound = new Audio('assets/F.mp3');
+sound.play();
+var sound = new Audio('assets/G.mp3');
+sound.play();
+var sound = new Audio('assets/hihat.mp3');
+sound.play();
+var sound = new Audio('assets/kick.mp3');
+sound.play();
+var sound = new Audio('assets/laugh-1.mp3');
+sound.play();
+var sound = new Audio('assets/laugh-2.mp3');
+sound.play();
+// Funktion für die Soundelemente
+function playSample() {
+}
 //# sourceMappingURL=Aufgabe7.js.map
---
+++ Aufgabe7/Aufgabe7.ts
@@ -0,0 +1,24 @@
+// Soundelemente
+
+var sound:HTMLAudioElement = new Audio('assets/A.mp3');
+sound.play();
+var sound:HTMLAudioElement = new Audio('assets/C.mp3');
+sound.play();
+var sound:HTMLAudioElement = new Audio('assets/F.mp3');
+sound.play();
+var sound:HTMLAudioElement = new Audio('assets/G.mp3');
+sound.play();
+var sound:HTMLAudioElement = new Audio('assets/hihat.mp3');
+sound.play();
+var sound:HTMLAudioElement = new Audio('assets/kick.mp3');
+sound.play();
+var sound:HTMLAudioElement = new Audio('assets/laugh-1.mp3');
+sound.play();
+var sound:HTMLAudioElement = new Audio('assets/laugh-2.mp3');
+sound.play();
+
+// Funktion für die Soundelemente
+
+function playSample() {
+
+}
